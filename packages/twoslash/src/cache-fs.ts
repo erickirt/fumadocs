fa3d4db6c7081c749709373c97fd1cbcf441f8f4
Fuma Nama <god63820869@gmail.com>
2025-04-26 15:44:10 +0800

    Twoslash: investigate Vercel Twoslash

--- packages/twoslash/src/cache-fs.ts
+++ packages/twoslash/src/cache-fs.ts
@@ -2,8 +2,8 @@
  * Original: https://github.com/shikijs/shiki/blob/main/packages/vitepress-twoslash/src/cache-fs.ts
  */
 import { createHash } from 'node:crypto';
-import { existsSync, mkdirSync, readFileSync, writeFileSync } from 'node:fs';
-import { join, resolve } from 'node:path';
+import * as fs from 'node:fs';
+import * as path from 'node:path';
 import type { TwoslashReturn } from 'twoslash';
 
 export interface FileSystemTypeResultCacheOptions {
@@ -40,25 +40,30 @@ export interface TwoslashTypesCache {
 export function createFileSystemTypesCache(
   options: FileSystemTypeResultCacheOptions = {},
 ): TwoslashTypesCache {
-  const dir = resolve(options.dir ?? '.next/cache/twoslash');
+  const dir = path.resolve(options.dir ?? '.next/cache/twoslash');
 
   return {
     init() {
-      mkdirSync(dir, { recursive: true });
+      try {
+        fs.mkdirSync('.next', { recursive: true });
+        fs.mkdirSync(dir, { recursive: true });
+      } catch (e) {
+        console.warn(e);
+      }
     },
     read(code) {
       const hash = createHash('SHA256').update(code).digest('hex').slice(0, 12);
-      const filePath = join(dir, `${hash}.json`);
-      if (!existsSync(filePath)) {
+      const filePath = path.join(dir, `${hash}.json`);
+      if (!fs.existsSync(filePath)) {
         return null;
       }
-      return JSON.parse(readFileSync(filePath, { encoding: 'utf-8' }));
+      return JSON.parse(fs.readFileSync(filePath, { encoding: 'utf-8' }));
     },
     write(code, data) {
       const hash = createHash('SHA256').update(code).digest('hex').slice(0, 12);
-      const filePath = join(dir, `${hash}.json`);
+      const filePath = path.join(dir, `${hash}.json`);
       const json = JSON.stringify(data);
-      writeFileSync(filePath, json, { encoding: 'utf-8' });
+      fs.writeFileSync(filePath, json, { encoding: 'utf-8' });
     },
   };
 }
